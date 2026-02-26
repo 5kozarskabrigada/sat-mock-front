5487c56ed28e0142bd160ddcece0d642eeb4e09b
Tomris Mammadova <mammadova.tomris@gmail.com>
2026-02-26 21:11:08 +0400

    .

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Logo({ className = "h-8" }: { className?: string }) {
   return (
     <img 
-      src="https://image2url.com/r2/default/images/1770968120228-c4943beb-29ce-40ef-a184-1e6904058998.jpg" 
+      src="https://image2url.com/r2/default/images/1772125793630-24416e69-e00e-4538-b1f3-b376f7fa528f.jpg"
       alt="ExamRoom Logo"
       className={`${className} object-contain`}
     />
