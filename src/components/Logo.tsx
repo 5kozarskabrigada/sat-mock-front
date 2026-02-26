325f67e2c583f2ad0f6dbd7f9e041724ab254f57
Tomris Mammadova <mammadova.tomris@gmail.com>
2026-02-26 21:13:51 +0400

    .

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo({ className = "h-8" }: { className?: string }) {
     <img 
       src="https://image2url.com/r2/default/images/1772125793630-24416e69-e00e-4538-b1f3-b376f7fa528f.jpg"
       alt="ExamRoom Logo"
-      className={`${className} object-contain`}
+      className={`${className} object-contain rounded-lg shadow-md`}
     />
   )
 }
