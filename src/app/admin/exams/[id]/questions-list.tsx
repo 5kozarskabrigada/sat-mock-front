d32cf31473b9f8695b1cbe5d840fefd1be965a7d
Tomris <mammadova.tomris@gmail.com>
2026-04-17 23:41:53 +0400

    Remove unused questions list import

--- src/app/admin/exams/[id]/questions-list.tsx
+++ src/app/admin/exams/[id]/questions-list.tsx
@@ -1,7 +1,6 @@
 
 'use client'
 
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import LatexRenderer from '@/components/ui/latex-renderer'
