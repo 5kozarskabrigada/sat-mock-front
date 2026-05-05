02a306d3cbd073fb6df494a037c11b9fffdb9f8f
Tomris <mammadova.tomris@gmail.com>
2026-05-06 00:49:11 +0400

    Remove basePath - use Nginx routing instead

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/1',
   productionBrowserSourceMaps: false,
   serverExternalPackages: [],
   experimental: {
