d9f8858388a1b3ee9bd38a4f368567b53792f5a9
Tomris <mammadova.tomris@gmail.com>
2026-05-19 22:40:21 +0400

    Fix logo not rendering - use local SVG instead of broken external URL

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,9 +4,9 @@ export default function Logo({ className = "h-8", withBorder = false }: { classN
   return (
     <div className={withBorder ? "bg-white p-3 rounded-lg border border-gray-200 shadow-sm" : ""}>
       <img 
-        src="https://image2url.com/r2/default/images/1772125793630-24416e69-e00e-4538-b1f3-b376f7fa528f.jpg"
+        src="/images/logo.svg"
         alt="ExamRoom Logo"
-        className={`${className} object-cover rounded-xl`}
+        className={`${className} object-contain`}
         style={{ aspectRatio: '4 / 1' }}
       />
     </div>
